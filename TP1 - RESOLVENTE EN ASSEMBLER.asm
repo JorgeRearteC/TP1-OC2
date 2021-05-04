1127dafee823760140b15294bfa8eaa5ba685db9
Jorge Rearte <jorgereartec@gmail.com>
2021-05-04 00:31:35 -0300

    Suma de variables

--- TP1 - RESOLVENTE EN ASSEMBLER.asm
+++ TP1 - RESOLVENTE EN ASSEMBLER.asm
@@ -4,6 +4,11 @@ extern printf
 section .data
     format db "numero: %d", 13,10,0  ;d: numero f: float s: string
     msg db 'Hello, world!', 0
+    
+    ; CAMBIAR A DQ PARA PUNTO FLOTANTE
+    vA db 1
+    vB db 2
+    vC db 3
 
 section .text
     global CMAIN
@@ -11,10 +16,16 @@ section .text
 CMAIN:
     mov ebp, esp; for correct debugging
  
-    mov eax, 14
+    mov eax, vA
+    mov ebx, vB
+    mov ecx, vC
     
+    mov edx, 0
+    
+    add edx, eax
+         
     ;PRINT CON LA PILA
-    push eax
+    push edx
     push format
     call printf
     add esp, 8
