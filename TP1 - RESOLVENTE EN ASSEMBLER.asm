af840f60384f65fc0e3833ca8743de7c6b92c2ce
Jorge Rearte <jorgereartec@gmail.com>
2021-05-06 01:22:20 -0300

    Verifico si tiene resultados

--- TP1 - RESOLVENTE EN ASSEMBLER.asm
+++ TP1 - RESOLVENTE EN ASSEMBLER.asm
@@ -4,7 +4,7 @@ section .data
     
     ; CAMBIAR A DQ PARA PUNTO FLOTANTE
     numA dq 4.0
-    numB dq 10.0
+    numB dq 1.0
     numC dq 3.0
     
     cos1 dq -4.0
@@ -26,46 +26,46 @@ section .text
 
 CMAIN:
     mov ebp, esp; for correct debugging
-    
-    ;CALCULO X1
+   
     
     fld qword [cos1] ; Se almacena en ST0 
-    
     fmul qword [numA] ; -4 * A
     fmul qword [numC] ; -4*A * C
-    
     fld qword [numB] ; B , -4*A*C
     fmul st0,st0 ; B*B , -4*A*C
-    
     faddp ; Suma todos los registros (B*B-4*A*C)
     
-    fsqrt ; √(B*B-4*A*C)
+    ; VALIDO SI TIENE RESULTADOS
+    ftst
+    fstsw ax
+    sahf
+    jb SIN_RESULTADO
     
+    
+    
+            
+    fsqrt ; √(B*B-4*A*C)
     fld qword [numB] ; B , √(B*B-4*A*C)
     fmul qword [cos2] ; -B , √(B*B-4*A*C)
-    
     fld qword [cos2] ; -1 , -B , √(B*B-4*A*C)
-    fmul st0,st2 ; -√(B*B-4*A*C), -B , √(B*B-4*A*C)  
-       
+    fmul st0,st2 ; -√(B*B-4*A*C), -B , √(B*B-4*A*C)     
     faddp ;  -B-√(B*B-4*A*C) , √(B*B-4*A*C)
-    
     fld qword [numA] ;  A , -B-√(B*B-4*A*C) , √(B*B-4*A*C)
     fmul qword [cos3] ; 2*A , -B-√(B*B-4*A*C) , √(B*B-4*A*C)
-    
     fdivp st1,st0  ;  (-B-√(B*B-4*A*C))/2*A , √(B*B-4*A*C)
-    
     fstp qword [resX2] ; GUARDO ST0 EN RESX2
 
-    ; VALIDAR SI TIENE RESULTADO
         
     
     
     ret
     
-SIN_RESULTADO:
+SIN_RESULTADO:;DEJO A LAS VARIABLES COMO 0
+    jmp FIN
+    ret
 
-FIN:
-
+FIN:;FIN EJECUCION
+    ret
 
     
     
